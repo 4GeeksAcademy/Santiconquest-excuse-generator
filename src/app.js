65561d78660481057cc5dad9369e363460968e65
Santiconquest <161180110+Santiconquest@users.noreply.github.com>
2024-03-12 22:31:09 +0000

    excuse generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.querySelector("#random-excuse").innerHTML = generateExcuse();
+  console.log("Hello");
+};
+
+let generateExcuse = () => {
+  let sujeto = ["Mi perro ", "Mi compu ", "Mi auto ", "Mi vecino ", "Mi gato "];
+  let accion = ["se desvanecio ", "se rompio ", "se cayo ", "se patino "];
+  let exageracion = [
+    "inesperadamente ",
+    "totalmente ",
+    "repentinamente ",
+    "increiblemente ",
+    "lamentablemente "
+  ];
+  let lugar = ["en mi casa", "en el supermercado", "en la calle", "en la ruta"];
+
+  let sujetoIndex = Math.floor(Math.random() * sujeto.length);
+  let accionIndex = Math.floor(Math.random() * accion.length);
+  let exageracionIndex = Math.floor(Math.random() * exageracion.length);
+  let lugarIndex = Math.floor(Math.random() * lugar.length);
+
+  return (
+    sujeto[sujetoIndex] +
+    accion[accionIndex] +
+    exageracion[exageracionIndex] +
+    lugar[lugarIndex]
+  );
 };
